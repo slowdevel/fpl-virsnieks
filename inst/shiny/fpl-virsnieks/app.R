58204fea5659321d2b05b378b029c48a44725e08
s.l.o.w. devel <devel@slowdevel.com>
2019-10-07 12:54:34 -0400

    added launch time

--- inst/shiny/fpl-virsnieks/app.R
+++ inst/shiny/fpl-virsnieks/app.R
@@ -2,18 +2,22 @@ fplVirsnieks::clr()
 
 library(shiny)
 
+
 fpl_state <- reactiveValues(
-  season = "----"
+  # set launch time when any session started
+  launch_time = reactiveVal(Sys.time())
 )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+  # splash screen
   waiter::use_waiter()
   , waiter::show_waiter_on_load(
     tagList(
       waiter::spin_fading_circles()
-      , "loading FPL Virsnieks..."
+      , br()
+      , p(style="color: white", "loading FPL Virsnieks...")
     )
   )
 
@@ -29,6 +33,9 @@ ui <- fluidPage(
                   min = 1,
                   max = 50,
                   value = 30)
+      , p("System Start Time: "), textOutput("launch_time", inline=F) #, tags$br()
+      , p("Gameweek Update Time: "), textOutput("gw_update_time", inline=F) #, tags$br()
+      , p("Live Update Time: "), textOutput("live_update_time", inline=F) #, tags$br()
     ),
 
     # Show a plot of the generated distribution
@@ -44,6 +51,10 @@ server <- function(input, output) {
   output$season <- renderText({
     fpl_state$season
   })
+
+  output$launch_time <- renderText({ format(fpl_state$launch_time(), "%Y-%m-%d %H:%M:%S")})
+  output$gw_update_time <- renderText({ format(fpl_state$gw_update_time, "%Y-%m-%d %H:%M:%S") })
+  output$live_update_time <- renderText({ format(fpl_state$live_update_time, "%Y-%m-%d %H:%M:%S") })
 
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
