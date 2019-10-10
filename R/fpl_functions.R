e31909767223a6b9deaee0238610c4a47f0b0747
s.l.o.w. devel <devel@slowdevel.com>
2019-10-10 11:44:04 -0400

    previous write test worked. now adding read test

--- R/fpl_functions.R
+++ R/fpl_functions.R
@@ -29,7 +29,7 @@ update_live_fpl <- function(state, full_update=F) {
     # player_gameweek_history <- fplVirsnieks::create_player_gameweek_history(gameweek)
 
     # assign once-per-week objects
-    # state$fantasy_key <- fplVirsnieks::read_dt("fantasy_key.csv")
+    state$fantasy_key <- fplVirsnieks::read_dt("fantasy_key.csv")
     # state$fantasy_snapshot <- fantasy_snapshot
     # state$fantasy_season_player_ix <- fantasy_season_player_ix
     # state$fantasy_gameweek_player_ix <- fantasy_gameweek_player_ix
